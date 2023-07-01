a5c6703950753e6162030b573dcc30c4c6ce8d20
AndrewLiii <114587322+AndrewLiii@users.noreply.github.com>
2023-07-01 15:28:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,21 +9,22 @@ import {tokenURL} from './backend'
 const hostURL = 'https://sheets.googleapis.com'
 
 export function GetSpreadsheet() {
-  fetch(hostURL + '/v4/spreadsheets/1S-xlfUClbfJzEbUC9evT6191E2VjxYCSoJ67F1nFO1A?key=AIzaSyBc7Ack1IVeI3tc9Ee8KOVxe9xG_RWNc10&access_token=ya29.a0Ael9sCPGWOI-7PYFuY1QbzMZihCZzjE0nB6EMVHAqjBf6p2AwWZDoMQey_xvNPD1sNc9wunwgSYHtXs7woHcbi3d9lCiRNGYy5byvMilW2qYLW7AinI9AJzSvvnJxCpdlQW8ea9CbFPvJ0K1jIVG6EJZN8PVaCgYKAX4SARISFQF4udJhWSooPazf2dTOfICav0B6kg0163')
+  fetch(hostURL + '/v4/spreadsheets/1S-xlfUClbfJzEbUC9evT6191E2VjxYCSoJ67F1nFO1A/?key=AIzaSyBc7Ack1IVeI3tc9Ee8KOVxe9xG_RWNc10&access_token=ya29.a0Ael9sCPGWOI-7PYFuY1QbzMZihCZzjE0nB6EMVHAqjBf6p2AwWZDoMQey_xvNPD1sNc9wunwgSYHtXs7woHcbi3d9lCiRNGYy5byvMilW2qYLW7AinI9AJzSvvnJxCpdlQW8ea9CbFPvJ0K1jIVG6EJZN8PVaCgYKAX4SARISFQF4udJhWSooPazf2dTOfICav0B6kg0163')
 }
 
 export function AppendSpreadsheet(emote, kendala1, kendala2, kendala3, kendala4, kendala5, kendala6, lainnya, nomorTelp, outlet, dateTime) {
   let values = [[emote, kendala1, kendala2, kendala3, kendala4, kendala5, kendala6, lainnya, nomorTelp, outlet, dateTime]]
-  fetch(hostURL + '/v4/spreadsheets/1S-xlfUClbfJzEbUC9evT6191E2VjxYCSoJ67F1nFO1A/values/A2:K1000:append?key=AIzaSyBc7Ack1IVeI3tc9Ee8KOVxe9xG_RWNc10&access_token='+ tokenURL +'&valueInputOption=USER_ENTERED',
+  let ranges = outlet + "!A2:K1000"
+  fetch(hostURL + '/v4/spreadsheets/1S-xlfUClbfJzEbUC9evT6191E2VjxYCSoJ67F1nFO1A/values/'+ outlet +'!A2:K1000:append?key=AIzaSyBc7Ack1IVeI3tc9Ee8KOVxe9xG_RWNc10&access_token='+ tokenURL +'&valueInputOption=USER_ENTERED',
   {
     method: 'POST',
     body: JSON.stringify({
       values: values,
-      range: "A2:K1000",
+      range: ranges,
       majorDimension: "DIMENSION_UNSPECIFIED"
     })
   })
-  window.location.replace("http://localhost:3000/survey/thankyou?outlet=" + outlet)
+  window.location.replace("https://zaplaundry.com/survey/thankyou?outlet=" + outlet)
 }
 
 function App() {
@@ -34,7 +35,7 @@ function App() {
           <Routes>
           <Route exact path='/survey' element={<Dashboard />} />
           <Route exact path='/survey/form' element={<Form />} />
-          <Route exact path='/survey/thankyou' element={<Thankyou />} />
+          <Route path='https://zaplaundry.com/survey/thankyou' element={<Thankyou />} />
           </Routes>
         </Router>
       </header>
