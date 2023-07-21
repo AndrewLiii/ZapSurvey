b1a040603e3f238b9a89352863254eb19d820f7f
AndrewLiii <114587322+AndrewLiii@users.noreply.github.com>
2023-07-21 15:19:31 +0700

    revisi

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Dashboard from './pages/dashboard';
 import Form from './pages/form'
 import Thankyou from './pages/thankyou'
 import {tokenURL} from './backend'
+import { Component } from 'react';
 
 const hostURL = 'https://sheets.googleapis.com'
+const id = '1S-xlfUClbfJzEbUC9evT6191E2VjxYCSoJ67F1nFO1A'
+const key = 'AIzaSyBc7Ack1IVeI3tc9Ee8KOVxe9xG_RWNc10'
+const access_token = 'ya29.a0Ael9sCPGWOI-7PYFuY1QbzMZihCZzjE0nB6EMVHAqjBf6p2AwWZDoMQey_xvNPD1sNc9wunwgSYHtXs7woHcbi3d9lCiRNGYy5byvMilW2qYLW7AinI9AJzSvvnJxCpdlQW8ea9CbFPvJ0K1jIVG6EJZN8PVaCgYKAX4SARISFQF4udJhWSooPazf2dTOfICav0B6kg0163'
 
 export function GetSpreadsheet() {
-  fetch(hostURL + '/v4/spreadsheets/1S-xlfUClbfJzEbUC9evT6191E2VjxYCSoJ67F1nFO1A/?key=AIzaSyBc7Ack1IVeI3tc9Ee8KOVxe9xG_RWNc10&access_token=ya29.a0Ael9sCPGWOI-7PYFuY1QbzMZihCZzjE0nB6EMVHAqjBf6p2AwWZDoMQey_xvNPD1sNc9wunwgSYHtXs7woHcbi3d9lCiRNGYy5byvMilW2qYLW7AinI9AJzSvvnJxCpdlQW8ea9CbFPvJ0K1jIVG6EJZN8PVaCgYKAX4SARISFQF4udJhWSooPazf2dTOfICav0B6kg0163')
+  fetch(hostURL + '/v4/spreadsheets/' + id + '/?key=' + key + '&access_token=' + access_token)
 }
 
 export function AppendSpreadsheet(emote, kendala1, kendala2, kendala3, kendala4, kendala5, kendala6, lainnya, nomorTelp, outlet, dateTime) {
   let values = [[emote, kendala1, kendala2, kendala3, kendala4, kendala5, kendala6, lainnya, nomorTelp, outlet, dateTime]]
   let ranges = outlet + "!A2:K1000"
-  fetch(hostURL + '/v4/spreadsheets/1S-xlfUClbfJzEbUC9evT6191E2VjxYCSoJ67F1nFO1A/values/'+ outlet +'!A2:K1000:append?key=AIzaSyBc7Ack1IVeI3tc9Ee8KOVxe9xG_RWNc10&access_token='+ tokenURL +'&valueInputOption=USER_ENTERED',
+  try{
+  fetch(hostURL + '/v4/spreadsheets/' + id + '/values/'+ outlet +'!A2:K1000:append?key=' + key + '&access_token='+ tokenURL +'&valueInputOption=USER_ENTERED',
   {
     method: 'POST',
     body: JSON.stringify({
@@ -24,6 +29,10 @@ export function AppendSpreadsheet(emote, kendala1, kendala2, kendala3, kendala4,
       majorDimension: "DIMENSION_UNSPECIFIED"
     })
   })
+  }
+  catch(e){
+    alert(e)
+  }
 }
 
 function App() {
--- src/pages/form.js
+++ src/pages/form.js
@@ -22,6 +22,10 @@ function Form() {
     const lainnya = document.getElementById('lainnya').value;
     const nomorTelp = document.getElementById('nomor-telp').value;
     //get all checkbox
+
+    const checkboxes = document.querySelectorAll(".cb")
+    console.log(checkboxes)
+
     const cb1 = document.getElementById('cb1').checked;
     const cb2 = document.getElementById('cb2').checked;
     const cb3 = document.getElementById('cb3').checked;
@@ -83,17 +87,17 @@ function Form() {
       <h2>Apa jenis kendala yang dialami?</h2>
         <form className='survey-form' onSubmit={handleForm} action={link} method="post">
           <div className='kendala-form'>
-          <input type="checkbox" id="cb1" name="cb1" value="Pegawai kurang sopan"></input>
+          <input type="checkbox" id="cb1" name="cb1" value="Pegawai kurang sopan" className='cb'></input>
           <label for="cb1">Pegawai kurang sopan</label> <br></br>
-          <input type="checkbox" id="cb2" name="cb2" value="Baju tertukar"></input>
+          <input type="checkbox" id="cb2" name="cb2" value="Baju tertukar" className='cb'></input>
           <label for="cb2">Baju tertukar</label> <br></br>
-          <input type="checkbox" id="cb3" name="cb3" value="Baju luntur"></input>
+          <input type="checkbox" id="cb3" name="cb3" value="Baju luntur" className='cb'></input>
           <label for="cb3">Baju luntur</label> <br></br>
-          <input type="checkbox" id="cb4" name="cb4" value="AC panas"></input>
+          <input type="checkbox" id="cb4" name="cb4" value="AC panas" className='cb'></input>
           <label for="cb4">AC panas</label> <br></br>
-          <input type="checkbox" id="cb5" name="cb5" value="Baju hilang"></input>
+          <input type="checkbox" id="cb5" name="cb5" value="Baju hilang" className='cb'></input>
           <label for="cb5">Baju hilang</label> <br></br>
-          <input type="checkbox" id="cb6" name="cb6" value="Barang hilang"></input>
+          <input type="checkbox" id="cb6" name="cb6" value="Barang hilang" className='cb'></input>
           <label for="cb6">Barang hilang</label> 
           <br></br>
           <input type="text" id="lainnya" name="lainnya" placeholder='Lainnya...'></input>
